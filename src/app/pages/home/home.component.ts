2dc1f6aac2f9b44cba0c47437fc63f60706f370e
DESKTOP-A129F57\Ruben Programador <rubeen1wp@gmail.com>
2025-05-28 14:26:48 +0200

    Inital things to work

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  products: Product[] = [];
+  error: string | null = null;
 
+  constructor(private productService: ProductService) {}
+
+  loadProducts(): void {
+    this.error = null; 
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        this.error = 'Failed to load products';
+        console.error(err);
+      }
+    });
+  }
 }
